b000398cd3ec5268d1f115dd5107bb729f5fa93d
Mo Alaoudi <141541084+NewMan2024@users.noreply.github.com>
2024-08-06 16:21:36 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -39,8 +39,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header>
         <div className="profile-icon" onClick={toggleProfileOptions}>
           <i className="fas fa-user-circle"></i>
           {profileOptionsVisible && (
