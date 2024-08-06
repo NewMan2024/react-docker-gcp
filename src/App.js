da64b72c211ba6958d489c93ece5e4072dab06a7
Mo Alaoudi <141541084+NewMan2024@users.noreply.github.com>
2024-08-06 16:04:11 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,9 @@
-
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Project Portfolio Website
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-
-/**
-import React, { useState } from 'react';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import './App.css';
-
-const App = () => {
   const [activeContainer, setActiveContainer] = useState('search');
   const [profileOptionsVisible, setProfileOptionsVisible] = useState(false);
 
@@ -44,7 +17,8 @@ const App = () => {
 
   return (
     <div className="App">
-      <header>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <div className="profile-icon" onClick={toggleProfileOptions}>
           <i className="fas fa-user-circle"></i>
           {profileOptionsVisible && (
@@ -147,6 +121,6 @@ const App = () => {
       </div>
     </div>
   );
-};
-**/
-//export default App;
+}
+
+export default App;
